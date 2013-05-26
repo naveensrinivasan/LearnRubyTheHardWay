9e26282cb790b9b2f84a366febff4c4dd3cd4973
naveensrinivasan <naveensrinivasan@yahoo.com>
2013-05-26 10:09:49 -0400

    Lesson2

---
+++ lesson2.rb
@@ -0,0 +1,9 @@
+# A comment , this is so you can read the program later.
+# Anything after the # is ignored in Ruby.
+
+puts "I could have code like this" # and comment after this is ignored.
+
+#You can also comment or "disable" a piece of code.
+# puts "This won't run"
+
+puts "This will run."
