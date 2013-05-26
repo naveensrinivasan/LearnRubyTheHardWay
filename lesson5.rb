dc652a4f3fac45d35b9d3768d8626ff58237a945
naveensrinivasan <naveensrinivasan@yahoo.com>
2013-05-26 13:21:12 -0400

    Lesson5 partial commit. Still would have to work on the extra credit!

---
+++ lesson5.rb
@@ -0,0 +1,13 @@
+name = 'Naveen Srinivasan'
+age = 22
+height = 64
+weight = 140
+eyes = 'brown'
+hair = 'black'
+teeth = 'White'
+
+puts "This is my name %s"%[name]
+puts "My height is %d." %height
+puts "My height and weight are %d %d" % [height, weight]
+puts "This is kind of tricky\n If I add %d, %d and %d I get %d." % [age, height, weight, age+height+weight]
+
