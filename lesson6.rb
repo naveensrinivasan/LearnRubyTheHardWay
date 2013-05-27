976b3a8db27b045a2823e0d15626051f5de32592
naveensrinivasan <naveensrinivasan@yahoo.com>
2013-05-26 20:28:10 -0400

    Lesson6 done!

---
+++ lesson6.rb
@@ -0,0 +1,17 @@
+x = "There are #{10} type of people"
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+hilarious = false
+joke_evaluation = "Isn't the joke funny? #{hilarious}"
+
+puts joke_evaluation
+
+w = "This is the left side of the string"
+e = "This is the right side of the string"
+
+puts w + e
