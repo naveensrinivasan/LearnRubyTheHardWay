061da89d5c28dab5e4aeb8400ee567c07cb70d13
naveensrinivasan <naveensrinivasan@yahoo.com>
2013-05-26 20:19:11 -0400

    Lesson5 done!

--- lesson5.rb
+++ lesson5.rb
@@ -11,3 +11,21 @@ puts "My height is %d." %height
 puts "My height and weight are %d %d" % [height, weight]
 puts "This is kind of tricky\n If I add %d, %d and %d I get %d." % [age, height, weight, age+height+weight]
 
+puts "I can see %i oranges" % 3
+
+puts "I can see %x Hexadecimal" % 3
+puts "I can see %o octal " % 3
+puts "I can see %b binary" % 3
+puts "I can see %f Floating" % 3
+
+# @param [Integer] inch
+def inches_to_centimeters(inch)
+  inch * 2.54
+end
+
+# @param [Integer] pound
+def pound_to_kilo(pound)
+  pound * 0.45
+end
+
+puts "%s is %d centimeters tall and weighs %d kilos" %[name, inches_to_centimeters(height), pound_to_kilo(weight)]
